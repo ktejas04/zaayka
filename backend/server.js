8b3c842ddc1cdf042e31749d3e9d6e7acbaf9110
Tejas <tejas22052004@gmail.com>
2024-06-15 09:36:40 +0530

    Changed few files

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json()); //The json response from FE to BE is parsed using this
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded form data
 
 // CORS Configuration
-app.use(cors());
+// app.use(cors());
 // app.use(cors({
 //   origin: "https://zaayka.vercel.app", // Add frontend here
 //   methods: ["GET", "POST", "PUT", "DELETE"],
